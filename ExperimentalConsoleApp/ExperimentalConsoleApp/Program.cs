4ef88b77d4b4c7b10b4593979780268e60cefe43
Dan Clarke <dan.clarke@brainjuicer.com>
2016-03-18 16:56:04 +0000

    added some ouput to the user

--- ExperimentalConsoleApp/ExperimentalConsoleApp/Program.cs
+++ ExperimentalConsoleApp/ExperimentalConsoleApp/Program.cs
@@ -1,9 +1,12 @@
 ﻿namespace ExperimentalConsoleApp
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("a slightly different output");
         }
     }
 }
